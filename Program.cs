334fa81f8426e16bae38ffe656ff89ee43562272
alvorijs <69315179+alvorijs@users.noreply.github.com>
2020-08-20 19:25:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Fibonace
+{
+    class Program
+    {
+        public static int fibonace(int n) {
+            int a = 0;
+            int b = 1;
+            //n represent number of fibonace series..
+            for (int i = 0; i < n; i++) {
+
+                int temp = a;
+                a = b;
+                b = temp + b;
+
+            }
+         
+             return a;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            //call fibonace function in main..
+
+            for (int i = 0; i < 15; i++) {
+                 //fibonace series upto 15
+                Console.Writeline(fibonace(i));
+
+            }
+            Console.Read();    
+        }
+        
+    }
+}                  
+
+
+        
+    
+
+    
+
+    
+
+
---
+++ fibonac.cs
@@ -0,0 +1,51 @@
+﻿using System;
+{}
+namespace Fibonace
+{
+    class Program
+   { static void Main(string[] args)
+    {
+
+    
+    {
+        public static int fibonace(int n) {
+            int a = 0;
+            int b = 1;
+            //n represent number of fibonace series..
+            for (int i = 0; i < n; i++) {
+
+                int temp = a;
+                a = b;
+                b = temp + b;
+
+            }
+         
+             return a;
+
+
+        }
+
+        {
+        static void Main(string[] args)
+            //call fibonace function in main..
+
+            for (int i = 0; i < 15; i++) {
+                 //fibonace series upto 15
+                Console.Writeline(fibonace(i));
+
+            }
+            Console. Read();    
+        }
+        
+    }
+}                  
+
+
+        
+    
+
+    
+
+    
+
+
